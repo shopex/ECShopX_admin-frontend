a57cba7de8c294555798b4af1e49de7763cef9f6
赵育坤 <zyk@zhaoyukundeMacBook-Pro.local>
2021-11-30 10:23:01 +0800

    fix:delete 商品类型

--- app/src/view/member/promotions/fullgift/list.vue
+++ app/src/view/member/promotions/fullgift/list.vue
@@ -33,7 +33,7 @@
             size="mini"
           ></el-date-picker>
         </el-col>
-        <el-col :md="4" :lg="8">
+        <!-- <el-col :md="4" :lg="8">
           <el-select
             v-model="params.item_type"
             @change="itemTypeChange"
@@ -46,7 +46,7 @@
             <el-option label="服务类商品" value="service"></el-option>
             <el-option label="实体类商品" value="normal"></el-option>
           </el-select>
-        </el-col>
+        </el-col> -->
         <el-col :md="4" :lg="8">
           <el-button size="mini" type="primary" icon="plus" @click="addActivityData"
             >添加满赠促销</el-button
