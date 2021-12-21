11d8d08ab3606b1ea3ad89d4373ad64f47aebae3
zyk <19121152559@163.com>
2021-12-21 17:39:43 +0800

    fix:涉及脱敏页面中的编辑需申请权限之后才可编辑，目前商家列表-编辑、会员列表-编辑基础信息/实物订单中订单类型下拉框中去除红框中的订单类型/编辑商品标签未获取前台是否展示的数据/【平台管理后台】和【店铺管理后台】模板中选择优惠券只能选择该平台下的优惠券

--- app/src/view/member/members/list.vue
+++ app/src/view/member/members/list.vue
@@ -339,7 +339,7 @@
             <template slot-scope="scope">
               <el-button type="text" @click="getDetail(scope.row.user_id)">详情</el-button>
               <el-button
-                v-if="$store.getters.login_type != 'distributor'"
+                v-if="$store.getters.login_type != 'distributor' && datapass_block == 0"
                 type="text"
                 @click="infoUpdate(scope.row)"
                 >编辑基础信息</el-button
