cf5607ba92960bfec9df10968dc5d2b1ff51d6d5
zyk <19121152559@163.com>
2022-01-19 13:59:14 +0800

    fix:1

--- app/src/view/base/shortmessage/ali_sms.vue
+++ app/src/view/base/shortmessage/ali_sms.vue
@@ -7,7 +7,7 @@
       <el-tab-pane label="基础配置" name="baseConfig">
         <base-config></base-config>
       </el-tab-pane>
-      <el-tab-pane label="配置管理" name="second">配置管理</el-tab-pane>
+      <el-tab-pane label="发送短信" name="second">配置管理</el-tab-pane>
       <el-tab-pane label="角色管理" name="third">角色管理</el-tab-pane>
       <el-tab-pane label="定时任务补偿" name="fourth">定时任务补偿</el-tab-pane>
     </el-tabs>
---
+++ app/src/view/base/shortmessage/cpn/send_sms.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="alisms_baseConfig">
+    <tips :title="tips"></tips>
+  </div>
+</template>
+
+<script>
+import tips from '@/components/tips'
+export default {
+    components:{
+        tips
+    },
+    data()
+}
+</script>
+
+<style>
+
+</style>
