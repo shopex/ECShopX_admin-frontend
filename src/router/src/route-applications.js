544bbb4763f16e679ba89e6fe9406c8ccc0c81ec
lukaijie <lukaijie@shopex.cn>
2022-08-04 17:51:09 +0800

    adapay router

--- src/router/src/route-applications.js
+++ src/router/src/route-applications.js
@@ -7,9 +7,69 @@ export default {
   component: Layout,
   children: [
     {
+      path: 'adapay/adapay_trades',
+      name: '分账明细',
+      component: () => import('@/view/mall/sub_account/sub_account'),
+      children: [
+        {
+          path: 'detail/:order_id',
+          component: () => import('@/view/mall/sub_account/detail')
+        }
+      ]
+    },
+    {
+      path: 'adapay/adapay_cash',
+      name: '提现',
+      component: () => import('@/view/mall/withdraw/withdraw')
+    },
+    {
+      path: 'adapay/adapay_cash_setting',
+      name: '提现设置',
+      component: () => import('@/view/mall/withdraw/withdraw_setting.vue')
+    },
+    {
+      path: 'adapay/adapay_merchant_info',
+      name: `开户信息`,
+      component: () => import('@/view/mall/accountopeninfo')
+    },
+    {
+      path: 'adapay/adapay_member_audit',
+      name: `开户审批`,
+      component: () => import('@/view/mall/marketing/openingapproval'),
+      children: [
+        {
+          path: 'approve/:id?',
+          component: () => import('@/view/mall/marketing/openingapproval/approve')
+        }
+      ]
+    },
+    {
+      path: 'adapay/adapay_merchant',
+      name: '商户设置',
+      component: () => import('@/view/base/setting/dealer'),
+      children: [
+        {
+          path: '/',
+          component: () => import('@/view/base/setting/dealer/account_info')
+        },
+        {
+          path: 'picture',
+          component: () => import('@/view/base/setting/dealer/upload_picture')
+        },
+        {
+          path: 'pay_setting',
+          component: () => import('@/view/base/setting/dealer/pay_setting')
+        },
+        {
+          path: 'netin',
+          component: () => import('@/view/base/setting/dealer/netin')
+        }
+      ]
+    },
+    {
       path: 'appsgroup/extapp',
       name: `扩展应用`,
-      component: () => import('@/view/applications/appsgroup/extapp'),
+      component: () => import('@/view/applications/appsgroup/extapp')
     }
   ]
 }
