4c7985ea8dbe9d59fe324941c14daa7e5a78ea70
xiezhigang <xiezhigang@shopex.cn>
2022-12-07 16:05:36 +0800

    添加店铺售后地址弹窗里的店铺不能去掉

--- src/view/mall/marketing/distributorAftersalesAddress.vue
+++ src/view/mall/marketing/distributorAftersalesAddress.vue
@@ -148,7 +148,7 @@
               :key="item.name"
               :disable-transitions="false"
               closable
-              @close="storeClose(rel_distributor_ids, index)"
+              @close="storeClose(index)"
             >
               {{ item.name }}
             </el-tag>
@@ -610,6 +610,9 @@ export default {
           this.fetchList()
         }
       })
+    },
+    storeClose(index){
+      this.rel_distributor_ids.splice(index,1)
     }
   }
 }
