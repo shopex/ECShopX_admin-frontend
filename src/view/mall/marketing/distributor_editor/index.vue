f94e28d7d713ca452faabf8c60178e807cadeec6
lukaijie <lukaijie@shopex.cn>
2023-01-04 13:48:40 +0800

    ECX-3998

--- src/view/mall/marketing/distributor_editor/index.vue
+++ src/view/mall/marketing/distributor_editor/index.vue
@@ -25,7 +25,11 @@
 <template>
   <div class="distributor-form">
     <!-- baseForm: {{ baseForm.distribution_type }} -->
-    <el-card v-if="is_normal" class="el-card--normal" header="店铺类型">
+    <el-card
+      v-if="is_normal && baseForm.distributor_self == 0"
+      class="el-card--normal"
+      header="店铺类型"
+    >
       <el-form ref="form" label-width="120px" :model="baseForm" :rules="rules">
         <el-form-item label="店铺类型" prop="distribution_type">
           <el-select v-model="baseForm.distribution_type" placeholder="请选择">
