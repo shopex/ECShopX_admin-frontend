ea3fd7e16b41856b9aee76e1552b365f75c6a7ec
lukaijie <lukaijie@shopex.cn>
2024-02-20 20:19:56 +0800

    feat: 装修模板添加储值链接

--- src/components/sp-picker/pickers/picker-link.vue
+++ src/components/sp-picker/pickers/picker-link.vue
@@ -80,7 +80,8 @@ export default {
       list: [
         { id: 'vipgrades', title: '会员开通' },
         { id: 'applyChief', title: '社区团长申请' },
-        { id: 'pointShop', title: '积分商城' }
+        { id: 'pointShop', title: '积分商城' },
+        { id: 'recharge', title: '储值卡' }
       ],
       multiple: this.value?.multiple ?? true
     }
