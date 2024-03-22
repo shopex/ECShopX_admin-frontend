36d28dc8152065081f6bf1863c6f6e9afa3bdb82
chenxin <398275325@qq.com>
2024-03-22 19:44:55 +0800

    附近商家可否直接加购

--- src/view/decorate/wgts/wgt_nearbyShop/config.js
+++ src/view/decorate/wgts/wgt_nearbyShop/config.js
@@ -8,6 +8,7 @@ export default {
     { label: '标题', key: 'title', component: 'input', value: '附近商家' },
     { label: '副标题', key: 'subtitle', component: 'input', value: '副标题' },
     { label: '组件间距', key: 'padded', component: 'switch', value: false },
+    { label: '直接加购', key: 'addCart', component: 'switch', value: false },
     {
       label: '商家',
       key: 'seletedTags',
@@ -63,7 +64,8 @@ export default {
           subtitle: 'subtitle',
           padded: 'padded',
           show_coupon: 'show_coupon',
-          navigation_display:'navigation_display'
+          navigation_display:'navigation_display',
+          addCart: 'addCart'
         })
       },
       seletedTags: 'seletedTags',
