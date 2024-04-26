3c90d76ad7b27f0f33785b7928f4d071e491673e
chenxin <398275325@qq.com>
2024-04-26 11:22:52 +0800

    店铺图片导航不存在lbs定位

--- src/view/decorate/wgts/wgt_imgNavigation/config.js
+++ src/view/decorate/wgts/wgt_imgNavigation/config.js
@@ -7,7 +7,9 @@ export default {
     { label: '标题', key: 'title', component: 'input', value: '标题' },
     { label: '副标题', key: 'subtitle', component: 'input', value: '副标题' },
     { label: '组件间距', key: 'padded', component: 'switch', value: true },
-    { label: 'lbs定位', key: 'navigation', component: 'switch', value: false },
+    { label: 'lbs定位', key: 'navigation', component: 'switch', value: false,isShow: function () {
+      return this.$store.getters.login_type == 'admin'
+    }},
     {
       label: '导航项',
       key: 'data',
