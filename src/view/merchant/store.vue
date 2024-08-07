99b6989b35bff2d69017ad392873484757e7f7a0
陈鑫 <398275325@qq.com>
2024-08-07 17:57:08 +0800

    商家自配送 给店铺开启同城配

--- src/view/merchant/store.vue
+++ src/view/merchant/store.vue
@@ -434,17 +434,25 @@ export default {
         {
           label: '送货方式',
           key: 'is_self_delivery',
-          type: 'radio',
-          options: [
-            { name: '商家自配送', label: true },
-            { name: '达达同城配', label: false }
+          // type: 'radio',
+          // options: [
+            // { name: '商家自配送', label: true },
+            // { name: '达达同城配', label: false }
             // { name: '闪送', label: 6 }
-          ],
-          isShow: ({ key }, value) => value.is_dada && this.dadaEnable
+          // ],
+          isShow: ({ key }, value) => value.is_dada,
+          component: ({ key }, value) => {
+            return (
+            <div style='margin-top: 14px;display:flex'>
+              <el-radio v-model={value[key]} label={true}>商家自配送</el-radio>
+              <el-radio v-model={value[key]} label={false} disabled={!this.dadaEnable}>达达同城配</el-radio>
+            </div>
+            )
+          }
         },
         {
           key: 'freight_time',
-          isShow: ({ key }, value) => value.is_self_delivery && value.is_dada && this.dadaEnable,
+          isShow: ({ key }, value) => value.is_self_delivery && value.is_dada,
           component: ({ key }, value) => {
             return (
               <div style='margin-left: 27px;display:flex'>
@@ -460,10 +468,10 @@ export default {
           key: 'business',
           type: 'select',
           options: [],
-          isShow: ({ key }, value) => !value.is_self_delivery && this.dadaEnable && value.is_dada,
+          isShow: ({ key }, value) => !value.is_self_delivery && value.is_dada,
           validator: (rule, value, callback) => {
             console.log('value:', value)
-            if (!this.form.is_self_delivery && this.dadaEnable && value.is_dada) {
+            if (!this.form.is_self_delivery && value.is_dada) {
               if (!value) {
                 callback(new Error('业务类型必填'))
               } else {
@@ -630,7 +638,7 @@ export default {
           item.options = typeList
         }
       })
-      this.dadaEnable = is_open === '1'
+      this.dadaEnable = is_open === '1'    
     },
     async getShansongInfo() {
       const { business_list, is_open } = await this.$api.dada.getShansongInfo()
@@ -645,7 +653,7 @@ export default {
           item.options = typeList
         }
       })
-      this.dadaEnable = is_open === '1'
+      this.dadaEnable = is_open === '1'     
     },
     async getStoreInfo() {
       const { distributor_id } = this.$route.query
