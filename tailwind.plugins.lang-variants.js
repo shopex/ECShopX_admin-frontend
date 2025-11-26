54b0d07fee762bc8a0454f403ad14522155327c7
lukaijie <lukaijie@shopex.cn>
2025-11-26 19:36:33 +0800

    style(scope): code formatting

--- tailwind.plugins.lang-variants.js
+++ tailwind.plugins.lang-variants.js
@@ -1,21 +1,3 @@
-// +----------------------------------------------------------------------
-// | ECShopX open source E-commerce
-// | ECShopX 开源商城系统
-// +----------------------------------------------------------------------
-// | Copyright (c) 2003-2025 ShopeX,Inc.All rights reserved.
-// +----------------------------------------------------------------------
-// | Corporate Website:  https://www.shopex.cn
-// +----------------------------------------------------------------------
-// | Licensed under the Apache License, Version 2.0
-// | http://www.apache.org/licenses/LICENSE-2.0
-// +----------------------------------------------------------------------
-// | The removal of shopeX copyright information without authorization is prohibited.
-// | 未经授权不可去除shopeX商派相关版权
-// +----------------------------------------------------------------------
-// | Author: shopeX Team <mkt@shopex.cn>
-// | Contact: 400-821-3106
-// +----------------------------------------------------------------------
-
 /**
  * Tailwind CSS 语言变体插件
  * 支持基于 HTML lang 属性的变体，例如：en-CN:pt-4, zh-CN:pt-2
